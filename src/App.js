77fa30eb3f7bce660643fd07765984eef845a0df
Kasiban <kasibanoy@gmal.com>
2019-01-06 16:52:18 -0800

    tic tac toe game

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Game from './components/Game';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Game />
+    );
+  }
+}
+
+export default App;
